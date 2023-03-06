e71ffbb74b2c074e8e20ab510ac7d89fdc74c92a
palashwaghmare <cdacjuhupalash@gmail.com>
2023-03-06 14:55:15 +0530

    Exercise-Videos

---
+++ fitbuddy/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './config/theme.config';
+
+import Dashboard from './Pages/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Switch>
+          {/*<Route exact path="/sigin">
+            <SignIn />
+          </Route>*/}
+          {/*<Route path="/sign-up">
+            <SignUp />
+          </Route>*/}
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </ThemeProvider>
+    </Router>
+  );
+}
+
+export default App;
